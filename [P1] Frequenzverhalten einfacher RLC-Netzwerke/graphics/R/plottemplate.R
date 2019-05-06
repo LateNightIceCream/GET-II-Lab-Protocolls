675f68692a21f1ec366b9edbf7ab0e7145c7c71b
LateNightIceCream <richardgruenert@live.com>
2019-05-06 22:43:26 +0000

    pepp

--- [P1] Frequenzverhalten einfacher RLC-Netzwerke/graphics/R/plottemplate.R
+++ [P1] Frequenzverhalten einfacher RLC-Netzwerke/graphics/R/plottemplate.R
@@ -4,6 +4,10 @@ options(scipen=10000)
 
 breaks <- 10^(-10:10)
 minor_breaks <- rep(1:9, 21)*(10^rep(-10:10, each=9))
+
+ybreaks = c(seq(0,1.0,0.1),round(1/sqrt(2),digits = 3));
+ybreaks = ybreaks[-8] # ybreaks without 0.7 (8th element)
+
 #data <- read.csv(file = "31.csv")
 
 #dataX <- data$x/100
@@ -11,13 +15,12 @@ minor_breaks <- rep(1:9, 21)*(10^rep(-10:10, each=9))
 
 #dat <- data.frame(x=dataX, y=dataB)
 
-C <- 0.001;
-R <- 10000;
+#C <- 0.001;
+#R <- 1;
 
-fg <- 1 / (2 * pi * R* C)
+#fg <- 1 / (2 * pi * R* C)
 
 rc_fun<- function(f) {1 / sqrt(1 + (f)^2)}
-#rc_fun2<- function(f) {1 / (1 + 4*pi*pi * f * f * 200*R * 0.5*R * 2*C * 2*C)}
 
 pdf("RC_LP.pdf")
 
@@ -26,11 +29,11 @@ p9 <- ggplot(data.frame(x = c(-4, 4)), aes(x = x)) +
         #geom_point(aes(dataX,dataB),  colour="deeppink1", shape="plus", size=2) +
         #geom_point(aes(-dataX,dataB),  colour="deeppink1", shape="plus", size=2) +
 
-        xlab("f / fg") +
+        xlab("log(f / fg)") +
         ylab("U2 / U1") +
-        scale_y_continuous(limits=c(0, 1)) +
+        scale_y_continuous(limits=c(0, 1), breaks = ybreaks) +
         scale_x_continuous(trans = "log10", breaks = breaks, minor_breaks = minor_breaks, limits=c(0.001, 1000))+
-        annotation_logticks()
+        annotation_logticks(sides = "b", colour = "grey69")
 
 p9 <- p9 + theme_bw()
 p9
