da547218cc8fa7f99aa7fad6469739bdb3730226
LateNightIceCream <richardgruenert@live.com>
2019-05-27 22:27:36 +0000

    update

--- [P3] Frequenzanalyse periodischer Signale/graphics/R/2_1/2_1_function.R
+++ [P3] Frequenzanalyse periodischer Signale/graphics/R/2_1/2_1_function.R
@@ -1,19 +1,13 @@
-#RC Tiefpassfilter
+# square wave plot
 
 library(ggplot2)
-library(RColorBrewer)
 
 options(scipen=10000)
-
-mod<-function(x,m)
-{
-    t1<-floor(x/m)
-    return(x-t1*m)
-}
 
 epsilon <- 10
 tau     <- 4
 T1      <- tau * 1.6180339887498948
+Xm      <- 1 #1.381966011
 
 output_width  <- 10
 output_height <- output_width * 0.6180339887498948
@@ -23,15 +17,15 @@ ylabel <- bquote( "x" )
 xlabel <- bquote( "t" )
 
 
-xbreaks <- c()
-loops <- 5
+xbreaks <- c(0, -tau/2, tau/2, -T1/2, T1/2, -T1, T1)
+xlabels <- c(0, bquote("-"~over(tau, 2)), bquote(over(tau, 2)), bquote("-"~over("T", 2)), bquote(over("T", 2)), "-T", "T")
 
-for(n in -loops:loops) {
-  xbreaks <- c(xbreaks, n*tau/2, n*T1/2)
-}
+#for(n in 1:3) {
+  #xbreaks <- c(xbreaks, n*T1)
+  #xlabels <- c(xlabels, bquote(.(n)~T))
+#}
 
-function_color <- "red"
-tempcolor      <- "blue"
+function_color <- "#0CB5DF"
 
 
 ####################################################################
@@ -39,29 +33,33 @@ tempcolor      <- "blue"
 p <- ggplot(data.frame(x=c(0,2), y=c(0,2)), aes(x=x)) +
 
         theme_minimal() + # maybe theme_bw()
-
+        theme(legend.position="none") +
         ggtitle("x(t)") +
 
-        theme(axis.title.y = element_text(angle = 0, vjust = .5)) + # rotate axis title
+        theme(text = element_text(size=16.180339887498948), axis.title.y = element_text(angle = 0, vjust = .5), panel.grid.minor.x = element_blank()) + # rotate axis title
 
-        xlab(xlabel)  +
-        ylab(ylabel)  +
+        xlab(xlabel) +
+        #ylab(ylabel)  +
 
-        scale_y_continuous(limits=c(0, 1.6180339887498948), breaks=c(0,1, 1.6180339887498948), labels=c(0, "Xm", "")) +
-        scale_x_continuous(limits=c(-epsilon, epsilon) , breaks=xbreaks) #labels=c(0, bquote("-"~over("T1",2)), bquote(over(-tau,2)), bquote(over(-tau,2)), bquote(over(tau,2))) )
+        scale_y_continuous(limits=c(0, 1.6180339887498948), breaks=c(0,Xm, 1.6180339887498948), labels=c(0, "Xm", "")) +
+        scale_x_continuous(limits=c(-epsilon, epsilon), breaks=xbreaks, labels=xlabels)
+        #scale_x_discrete(limits=1, breaks=c(1,2,3)) #labels=c(0, bquote("-"~over("T1",2)), bquote(over(-tau,2)), bquote(over(-tau,2)), bquote(over(tau,2))) )
 
 delta <- T1
 
 for (n in -1:1) {
 
   # solid horizontal lines
-  p <- p + geom_segment( x = -tau/2 + n*delta, y = 1, xend = tau/2  + n*delta,  yend = 1, color=tempcolor, linetype="solid") +
-      geom_segment( x = -T1/2  + n*delta, y = 0, xend = -tau/2 + n*delta,  yend = 0, color=tempcolor, linetype="solid") +
-      geom_segment( x = T1/2   + n*delta, y = 0, xend = tau/2  + n*delta,  yend = 0, color=tempcolor, linetype="solid")
+  p <- p + geom_segment( x = -tau/2 + n*delta, y = Xm, xend = tau/2  + n*delta,  yend = Xm, color=function_color, linetype="solid") +
+      geom_segment( x = -T1/2  + n*delta, y = 0, xend = -tau/2 + n*delta,  yend = 0, color=function_color, linetype="solid") +
+      geom_segment( x = T1/2   + n*delta, y = 0, xend = tau/2  + n*delta,  yend = 0, color=function_color, linetype="solid")
+
+  # test segment
+  #p <- p + geom_segment( x = T1/2, y = 0, xend = T1/2-tau/2 + T1/2, yend = 0, aes(color="blue"), linetype="solid")
 
   # dashed vertical lines
-  p <- p + geom_segment( x = -tau/2 + n*delta, y = 0, xend = -tau/2 + n*delta, yend = 1, color=function_color, linetype="dashed")+
-           geom_segment( x =  tau/2 + n*delta, y = 0, xend = tau/2 + n*delta,  yend = 1, color=function_color, linetype="dashed")
+  p <- p + geom_segment( x = -tau/2 + n*delta, y = 0, xend = -tau/2 + n*delta, yend = Xm, color=function_color, linetype="dashed")+
+           geom_segment( x =  tau/2 + n*delta, y = 0, xend = tau/2 + n*delta,  yend = Xm, color=function_color, linetype="dashed")
 }
 
 pdf("2_1_function.pdf", width = output_width, height = output_height, )
