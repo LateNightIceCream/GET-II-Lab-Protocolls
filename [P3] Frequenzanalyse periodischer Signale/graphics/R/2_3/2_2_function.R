1be7b27c4e2d53c4c03c8beb12b6cb348881c8bd
LateNightIceCream <richardgruenert@live.com>
2019-06-03 16:26:49 +0000

    update

--- [P3] Frequenzanalyse periodischer Signale/graphics/R/2_3/2_2_function.R
+++ [P3] Frequenzanalyse periodischer Signale/graphics/R/2_3/2_2_function.R
@@ -4,17 +4,25 @@ library(ggplot2)
 
 options(scipen=10000)
 
-epsilon <- 50 * 10^(-6)
-Xm      <- 1
-D       <- 0.5
-T1      <- 25 * 10^(-6)
-tau     <- D * T1
+epsilon <- 2 * 10^(-3)
+T1      <- 1/1000
+Xm <- 1
 
+
+U0m <- 1
+R1  <- 400
+R2  <- 1000
+C   <- 10^(-6)
+
+U2m    <- U0m * R2 / (sqrt( (R1+R2)^2 + (2*pi/T1 * C * R1 * R2)^2 ))
+phase2 <- 0 - atan( (2*pi/T1 * C * R1 * R2) / (R1+R2) )
+
+print(phase2 * 180/pi)
 
 output_width  <- 10
 output_height <- output_width * 0.6180339887498948
 
-ylabel <- bquote( "x" )
+ylabel <- bquote( "" )
 xlabel <- bquote( "t" )
 
 
@@ -24,6 +32,12 @@ xlabels <- c(0, bquote("-"~over("T"[1], 2)), bquote(over("T"[1], 2)), bquote(ove
 fun <- function(t) {
 
   abs(Xm*cos(pi/T1*t))
+
+}
+
+fun2 <- function(t) {
+
+  U2m * cos(2*pi/T1 * t + phase2) + 0.455
 
 }
 
@@ -39,18 +53,19 @@ p <- ggplot(data.frame(x=c(0,2), y=c(0,2)), aes(x=x)) +
         theme(legend.position="none", plot.title = element_text(color = "gray21", size=1.6180339887498948^5), plot.subtitle = element_text(color = "grey80", size=1.6180339887498948^5)) +
         ggtitle(bquote("x(t)")) +
 
-        stat_function(fun = fun, colour   = function_color, n=2000)+
+        stat_function(fun = fun, colour   = indie_wave_color, n=2000)+
+        stat_function(fun = fun2, colour  = function_color, n=2000)+
 
 
         theme(text = element_text(size=16.180339887498948), axis.title.y = element_text(angle = 0, vjust = .5), panel.grid.minor.x = element_blank()) + # rotate axis title
 
         xlab(xlabel) +
-        ylab("")  +
+        ylab(ylabel)  +
 
-        scale_y_continuous(limits=c(-0.145898034, 1.6180339887498948), breaks=c(0,Xm, 1.6180339887498948), labels=c(0, bquote("X"["m"]), "")) +
+        scale_y_continuous(limits=c(-0.145898034, 1.6180339887498948), breaks=c(0,Xm, 1.6180339887498948), labels=c(0, bquote("U"["0m"]), "")) +
         scale_x_continuous(limits=c(-epsilon, epsilon), breaks=xbreaks, labels=xlabels)
         #scale_x_discrete(limits=1, breaks=c(1,2,3)) #labels=c(0, bquote("-"~over("T1",2)), bquote(over(-tau,2)), bquote(over(-tau,2)), bquote(over(tau,2))) )
 
 
-pdf("2_2_function.pdf", width = output_width, height = output_height, )
+pdf("2_3_function.pdf", width = output_width, height = output_height, )
 p
